3daecebae5e0ab68a0eeaf8e9e50f5f418bf190b
2fstoremenswear-dev <2fstoremenswear@gmail.com>
2025-11-16 18:50:23 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "casal-gamificado-v1";
+
+const URLS_TO_CACHE = [
+  "/",
+  "/manifest.json",
+  "/favicon.ico"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request);
+    })
+  );
+});
